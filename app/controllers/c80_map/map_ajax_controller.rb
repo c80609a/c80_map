66787bb4ac8a4c8d72aac50f7f607bae4437b653
C80609A <c080609a@gmail.com>
2016-06-20 15:29:06 +0300

    develop: можно создавать и сохранять areas.

--- app/controllers/c80_map/map_ajax_controller.rb
+++ app/controllers/c80_map/map_ajax_controller.rb
@@ -2,10 +2,17 @@ module C80Map
   class MapAjaxController < ApplicationController
 
     def save_map_data
-      puts "<MapAjaxController.save_map_data> #{params[:buildings]}"
+      Rails.logger.debug "<MapAjaxController.save_map_data> params = #{params}"
 
       #{ "buildings"=>{"0"=>{"coords"=>["2496.5894495412845",...]}} }
       #{ "areas"=>{"0"=>{"coords"=>["2496.5894495412845",...]}} }
+
+      # ЗДАНИЯ
+      # в случае успеха - вернём id созданного здания,
+      # тогда js обновит id и перенесёт здание из массива "новые здания" (drawn_buildings).
+      # в случае неудачи - вернём описание ошибки
+      # завершаем всё обновленным locations.json, который Map возьмёт
+      # и положит в data
 
       result = {
           areas: [],
@@ -16,37 +23,42 @@ module C80Map
       if params[:buildings].present?
         params[:buildings].each_key do |key|
           new_building_options = params[:buildings][key]
-          puts "<MapAjaxController.save_map_data> new_building_options = #{new_building_options}"
-          puts "<MapAjaxController.save_map_data> new_building_options[:coords] = #{new_building_options[:coords]}"
+          # puts "<MapAjaxController.save_map_data> new_building_options = #{new_building_options}"
+          # puts "<MapAjaxController.save_map_data> new_building_options[:coords] = #{new_building_options[:coords]}"
           b = Building.new({ coords: new_building_options[:coords].join(',') })
-
-          # в случае успеха - вернём id созданного здания,
-          # тогда js обновит id и перенесёт здание из массива "новые здания" (drawn_buildings).
-          # в случае неудачи - вернём описание ошибки
-          # завершаем всё обновленным locations.json, который Map возьмёт
-          # и положит в data
 
           if b.valid?
             b.save
-            result[:buildings] << { id: b.id }
+            result[:buildings] << { id: b.id, old_temp_id: new_building_options["id"] }
           else
             result[:buildings] << { id: new_building_options.id, status: 'error' }
           end
         end
       end
 
-      # if params[:areas].present?
-      #   params[:areas].each_with_key do |new_area_options,key|
-          #
-          # if b.present?
-          #
-          # end
-        # end
-      # end
+      if params[:areas].present?
+        params[:areas].each_key do |key|
+          new_area_options = params[:areas][key]
+          # Rails.logger.debug "<MapAjaxController.save_map_data> new_area_options = #{new_area_options}"
+          # puts "<MapAjaxController.save_map_data> new_area_options[:coords] = #{new_area_options[:coords]}"
+          a = Area.new({
+                           coords: new_area_options[:coords].join(','),
+                           building_id: new_area_options[:parent_building_id]
+                       })
+
+          if a.valid?
+            a.save
+            result[:areas] << { id: a.id, old_temp_id: new_area_options["id"] }
+          else
+            result[:areas] << { id: new_area_options.id, status: 'error' }
+          end
+        end
+      end
+
       puts "<MapAjaxController.save_map_data> result = #{result.to_json}"
 
       respond_to do |format|
-        format.json { render json: params }
+        format.json { render json: result }
       end
     end
 
--- app/models/c80_map/building.rb
+++ app/models/c80_map/building.rb
@@ -9,66 +9,7 @@ module C80Map
     private
 
     def update_json
-
-      locations_path = Rails.root.join("public", "locations.json")
-      locs = File.read(locations_path)
-      puts "<update_json> #{ Rails.root.join("public", "locations.json") }"
-
-      locs_hash = JSON.parse(locs)
-
-
-      # и заполняем объектами из собранного массива
-
-      # обходим все Building и составляем массив вида
-      # [{id, object_type=object_type, coords, building_hash, img, childs:[<areas>]},..]
-      buildings_to_location_json = []
-      Building.all.each do |b|
-        ob = {
-            id: b.id,
-            object_type: 'building',
-            coords: b.coords.split(","),
-            building_hash: {
-                id: 2,
-                title: "Здание 2",
-                props: {
-                    square: "1234 кв.м.",
-                    square_free: "124 кв. м",
-                    floor_height: "6 кв. м",
-                    column_step: "2 м",
-                    gate_type: "рaспашные",
-                    communications: "Интернет, электричество, водоснабжение",
-                    price: "от 155 руб/кв.м в месяц"
-                }
-            },
-            img: {
-                bg: {
-                    src: b.img_bg.url
-                },
-                overlay: {
-                    src: b.img_overlay.url
-                }
-            },
-            childs: []
-        }
-        buildings_to_location_json << ob
-      end
-
-      # после чего удаляем из locs_hash все object_type=building
-      # locs_hash["childs"].each do |child|
-      #   if child["object_type"] == 'building'
-      #     delete locs_hash["childs"]
-      #   end
-      # end
-
-      # просто заменяем всех детей
-      locs_hash["childs"] = buildings_to_location_json
-
-      puts "<Building.update_json> #{locs_hash}"
-
-      File.open(locations_path, 'w') do |f|
-        f.write(locs_hash.to_json)
-      end
-
+      MapJson.update_json
     end
 
   end
